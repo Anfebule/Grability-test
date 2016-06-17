14997eaac10197eec0b10c3efe726aeb1b346d7d
Ing-0-0013 <Ing-0-0013@10.67.144.134>
2016-06-17 11:44:31 -0500

    Se eliminan importaciones no necesairas

--- test/controlador/CuboTest.java
+++ test/controlador/CuboTest.java
@@ -5,12 +5,8 @@
  */
 package controlador;
 
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import org.junit.Before;
-import org.junit.BeforeClass;
 
 /**
  *
